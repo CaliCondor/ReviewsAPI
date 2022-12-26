3fa2e05e9724d73a5213ae5583b0ce2ef94cb9a4
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-12-26 15:45:01 -0500

    More robust typing, hopefully finish characteristics

--- src/db/productReviews.ts
+++ src/db/productReviews.ts
@@ -3,29 +3,36 @@ mongoose.connect('mongodb://localhost:27017/reviews');
 
 /* INTERFACES FOR SCHEMAS */
 
-interface IPhoto {
-  id: Number,
-  url: String,
-};
+export interface IPhoto {
+  id: number,
+  url: string,
+}
 
 interface IReview {
-  review_id: Number,
-  rating: Number,
-  summary: String,
-  recommended: Boolean,
-  response: String,
-  body: String,
-  date: String,
-  name: String,
-  helpfulness: Number,
-  reported: Boolean,
+  review_id: number,
+  rating: number,
+  summary: string,
+  recommended: boolean,
+  response: string,
+  body: string,
+  date: string,
+  name: string,
+  helpfulness: number,
+  reported: boolean,
   photos?: Array<IPhoto>,
-};
+}
 
-interface IProduct {
-  product_id: Number,
+export interface ICharacteristic {
+  characteristic_id: number,
+  name: string,
+  values: number[],
+}
+
+export interface IProduct {
+  product_id: number,
   reviews: IReview[],
-};
+  characteristics: ICharacteristic[],
+}
 
 
 /* SCHEMA DEFINITIONS */
@@ -45,10 +52,18 @@ const reviewSchema = new mongoose.Schema<IReview>({
   photos: Array<IPhoto>,
 });
 
+// characteristic documents store a characteristic as well as all values
+const characteristicSchema = new mongoose.Schema<ICharacteristic>({
+  characteristic_id: Number,
+  name: String,
+  values: Array<number>,
+});
+
 // product documents store the product id, as well as all the review sub documents
 const productSchema = new mongoose.Schema<IProduct>({
   product_id: {type: Number, index: { unique: true }},
   reviews: [reviewSchema],
+  characteristics: [characteristicSchema],
 });
 
 const Product = mongoose.model<IProduct>('Product', productSchema);
--- src/etl/mongoEtl.ts
+++ src/etl/mongoEtl.ts
@@ -1,56 +1,39 @@
 import * as fs from 'fs';
 import process from 'process';
 import { parse } from 'csv-parse';
-import Product from '../db/productReviews';
+import Product, { IProduct, IPhoto, ICharacteristic } from '../db/productReviews';
 
 // generate basic products table, don't need to run every time
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const generateProducts = async () => {
   await Product.collection.drop();
-  let index = 0;
-  let per = 0;
-  const ids: any = {};
+  const ids: Record<string, IProduct> = {};
   const parser = parse();
   console.log('generating product IDs...');
-  fs.createReadStream('./dist/reviews.csv', {encoding: 'utf8'})
+  fs.createReadStream('./build/etl/reviews.csv', {encoding: 'utf8'})
     .pipe(parser)
-    .on('data', (row: any) => {
-      index++;
-      if (Math.floor((index / 5774951) * 100) > per) {
-        per = Math.floor((index / 5774951) * 100);
-        // percent way through .csv
-        console.log(per + '%');
-      }
-
+    .on('data', (row: string[]) => {
       if (!Number.isNaN(parseInt(row[1]))) {
         ids[row[1]] = {
           product_id: parseInt(row[1]),
           reviews: [],
+          characteristics: [],
         };
       }
     })
     .on('end', () => {
+      console.log('finished generating product ids!')
       insertReviews(ids);
     });
 }
 
-const insertReviews = (obj: any) => {
+// insert individual review subdocuments
+const insertReviews = (obj: Record<string, IProduct>) => {
   console.log('generating reviews...');
   const parser = parse();
-  let index = 0;
-  let per = 0;
-  fs.createReadStream('./dist/reviews.csv', {encoding: 'utf8'})
+  fs.createReadStream('./build/etl/reviews.csv', {encoding: 'utf8'})
     .pipe(parser)
     .on('data', (row: string[]) => {
-      index++;
-      if (index % 100000 === 0) {
-        console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
-      }
-      if (Math.floor((index / 5774951) * 100) > per) {
-        per = Math.floor((index / 5774951) * 100);
-        // percent way through .csv
-        console.log(per + '%');
-      }
-
       // push review to correct product
       if (!Number.isNaN(parseInt(row[1]))) {
         obj[parseInt(row[1])].reviews.push({
@@ -70,37 +53,35 @@ const insertReviews = (obj: any) => {
     })
     .on('end', async () => {
       // const arr: IProduct[] = [];
-      console.log('Inserting reviews!')
+      console.log('inserting reviews...')
       let index = 0;
       let per = 0;
-      let len = Object.keys(obj).length;
+      const len = Object.keys(obj).length;
       for (const key in obj) {
         // tracking
         index++;
-        if (index % 100000 === 0) {
-          console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
-        }
         if (Math.floor((index / len) * 100) > per) {
           per = Math.floor((index / len) * 100);
-          // percent way through .csv
           console.log(per + '%');
+          console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
         }
 
         await Product.create(obj[key]);
         delete obj[key];
       }
-      console.log('finished inserting!');
+      console.log('finished inserting reviews!');
       console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
       insertPhotos();
     });
 }
 
+// insert photos into review subdocuments
 const insertPhotos = () => {
   let index = 0;
   let per = 0;
-  let obj: any = {};
-  console.log('reading csv...');
-  fs.createReadStream('./dist/reviews_photos.csv', {encoding: 'utf8'})
+  const obj: Record<string, IPhoto[]> = {};
+  console.log('generating photos...');
+  fs.createReadStream('./build/etl/reviews_photos.csv', {encoding: 'utf8'})
     .pipe(parse())
     .on('data', (row: string[]) => {
       if (!Number.isNaN(parseInt(row[1]))) {
@@ -112,16 +93,14 @@ const insertPhotos = () => {
       }
     })
     .on('end', async () => {
-      console.log('writing photos to db...');
-      let len = Object.keys(obj).length;
+      console.log('insertings photos...');
+      const len = Object.keys(obj).length;
       for (const key in obj) {
         index++;
-        if (index % 100000 === 0) {
-          console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
-        }
         if (Math.floor((index / len) * 100) > per) {
           per = Math.floor((index / len) * 100);
           console.log(per + '%');
+          console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
         }
 
         await Product.findOneAndUpdate(
@@ -131,25 +110,45 @@ const insertPhotos = () => {
           }});
         delete obj[key];
       }
+      console.log('finished inserting photos!');
       readCharacteristics();
     });
 };
 
+// initialize characteristics
 const readCharacteristics = () => {
-  const obj: any = {};
-  console.log('applying characteristics to products...');
-  fs.createReadStream('./dist/characteristics.csv', {encoding: 'utf8'})
+  const obj: Record<string, ICharacteristic[]> = {};
+  console.log('generating characteristics...');
+  fs.createReadStream('./build/etl/characteristics.csv', {encoding: 'utf8'})
     .pipe(parse())
     .on('data', (row) => {
       if (!Number.isNaN(parseInt(row[0]))) {
-        obj[row[1]] = {
+        if (obj[row[1]] === undefined) obj[row[1]] = [];
+        obj[row[1]].push({
           name: row[2],
-          id: row[0],
-        };
+          characteristic_id: parseInt(row[0]),
+          values: [],
+        });
       }
     })
-    .on('end', () => {
+    .on('end', async () => {
+      console.log('inserting characteristics...');
+      const len = Object.keys(obj).length;
+      let index = 0;
+      let per = 0;
 
+      for (const key in obj) {
+        index++;
+        if (Math.floor((index / len) * 100) > per) {
+          per = Math.floor((index / len) * 100);
+          console.log(per + '%');
+          console.log('memory usage: ', Math.round((process.memoryUsage().rss) / 1024 / 1024), 'MB');
+        }
+
+        await Product.findOneAndUpdate({ product_id: key }, { $set: { characteristics: obj[key] }});
+        delete obj[key];
+      }
+      console.log('finished inserting characteristics!');
     });
 };
 
