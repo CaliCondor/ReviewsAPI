a6b701ed4cf8e72da86658807897971d0cc83acc
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-12-28 00:57:02 -0500

    Trying to figure out what's going wrong

--- src/server/getReviews.ts
+++ src/server/getReviews.ts
@@ -10,23 +10,29 @@ const getReviews = async (req: Request, res: Response) => {
     return;
   }
 
-  // parse query params
-  const product_id = parseInt(req.query.product_id as string);
-  const page = req.query.page ? parseInt(req.query.page as string) : 1;
-  const count = req.query.count ? parseInt(req.query.count as string) : 5;
-  // TODO: figure out how the sorting works
-  // const { sort } = req.query;
+  try {
+    // parse query params
+    const product_id = parseInt(req.query.product_id as string);
+    const page = req.query.page ? parseInt(req.query.page as string) : 1;
+    const count = req.query.count ? parseInt(req.query.count as string) : 5;
+    // TODO: figure out how the sorting works
+    // const { sort } = req.query;
 
-  // get all the reviews, then filter the reviews by the page and count
-  const query = await Product.findOne({ product_id: product_id }, 'reviews');
-  const reviews = query?.reviews.slice((page - 1) * count, page * count);
+    // get all the reviews, then filter the reviews by the page and count
+    const query = await Product.findOne({ product_id: product_id }, 'reviews');
+    const reviews = query?.reviews.slice((page - 1) * count, page * count);
 
-  res.send({
-    product: '' + product_id,
-    page,
-    count,
-    results: reviews
-  });
+    res.send({
+      product: '' + product_id,
+      page,
+      count,
+      results: reviews
+    });
+  } catch {
+    res.status(500);
+    res.send('Internal server error');
+  }
+
 };
 
 export default getReviews;
