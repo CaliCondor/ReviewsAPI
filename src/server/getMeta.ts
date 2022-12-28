828b7b05783119e41b81bf36c0628a3e2e33c2a5
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-12-28 00:19:24 -0500

    Complete most of GET /reviews/meta endpoint

--- src/server/getMeta.ts
+++ src/server/getMeta.ts
@@ -1,29 +1,61 @@
 import { Request, Response } from 'express';
 import Product from '../db/productReviews';
 
+// characteristics send by API are shaped differently than the DB stores them
+interface Characteristic {
+  id: number,
+  value: string,
+}
+
 const getMeta = async (req: Request, res: Response) => {
+  // check to ensure request has a product id
+  if (!req.query.product_id) {
+    res.status(422);
+    res.send('Error: invalid product_id provided');
+    return;
+  }
+
+  // parse query params for product id
   const product_id = parseInt(req.query.product_id as string);
+
+  // query DB for product
   const product = await Product.findOne({ product_id: product_id });
 
-  // meta objects, edited by iterating through the product's reviews
-  const ratings = {
+  // meta objects containers
+  const ratings: Record<string, string> = {
     '1': '0',
     '2': '0',
     '3': '0',
     '4': '0',
     '5': '0',
   };
-  const recommended = {
+  const recommended: Record<string, string> = {
     'false': '0',
     'true': '0',
   };
-  const characteristics = product?.characteristics;
+
+  // read characteristics from db, calculate average value
+  const characteristics: Record<string, Characteristic> = {};
+  if (product?.characteristics) {
+    product.characteristics.forEach((c) => {
+      characteristics[c.name] = {
+        id: c.characteristic_id,
+        value: '' + (c.values.reduce((a:number, b:number) => a + b, 0) / c.values.length)
+      };
+    });
+  }
+
+  // edit containers based on queried data
+  product?.reviews.forEach((review) => {
+    ratings['' + review.rating] = '' + (parseInt(ratings['' + review.rating]) + 1);
+    recommended['' + review.recommended] = '' + parseInt(recommended['' + review.recommended] + 1);
+  });
 
   res.send({
-    'product_id': product_id + '',
-    'ratings': ratings,
-    'recommended': recommended,
-    'characteristics': characteristics
+    product_id: '' + product_id,
+    ratings,
+    recommended,
+    characteristics
   });
 };
 
