0ecd8b2d49d774ccc95d00b345867ac2a44dd36e
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-03 17:57:51 -0500

    Flesh out first test

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -5,5 +5,8 @@ describe('GET reviews', () => {
   it('sends a basic response', async () => {
     const { data } = await axios.get('http://localhost:3000/reviews?product_id=1');
     expect(data).toBeDefined();
+    expect(data.results).toBeDefined();
+    expect(data.results.length).toEqual(3);
+    expect(data.page).toEqual(1);
   });
 });
