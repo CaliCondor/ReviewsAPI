90c4a0fd09c93ead3892342b9ff882e89a8fac7c
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-04 14:51:38 -0500

    Basic getReviews tests

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -9,4 +9,16 @@ describe('GET reviews', () => {
     expect(data.results.length).toEqual(3);
     expect(data.page).toEqual(1);
   });
+
+  it('returns the requested amount of reviews', async () => {
+    const { data } = await axios.get('http://localhost:3000/reviews?product_id=2&count=2');
+    expect(data.count).toEqual(2);
+    expect(data.results.length).toEqual(2);
+  });
+
+  it('paginates requests', async () => {
+    const { data } = await axios.get('http://localhost:3000/reviews?product_id=2&count=2&page=2');
+    expect(data.page).toEqual(2);
+    expect(data.results.length).toEqual(2);
+  });
 });
