33489e9661bc7868b2e42101dc85631325cec537
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-04 15:26:01 -0500

    Basic meta test

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -3,7 +3,8 @@ const axios = require('axios').default;
 
 describe('GET reviews', () => {
   it('sends a basic response', async () => {
-    const { data } = await axios.get('http://localhost:3000/reviews?product_id=1');
+    const { data, status } = await axios.get('http://localhost:3000/reviews?product_id=1');
+    expect(status).toEqual(200);
     expect(data).toBeDefined();
     expect(data.results).toBeDefined();
     expect(data.results.length).toEqual(3);
@@ -20,5 +21,17 @@ describe('GET reviews', () => {
     const { data } = await axios.get('http://localhost:3000/reviews?product_id=2&count=2&page=2');
     expect(data.page).toEqual(2);
     expect(data.results.length).toEqual(2);
+  });
+});
+
+describe('GET meta', () => {
+  it('sends a basic response', async () => {
+    const { data, status } = await axios.get('http://localhost:3000/reviews/meta?product_id=1');
+    expect(status).toEqual(200);
+    expect(data).toBeDefined();
+    expect(data.ratings).toBeDefined();
+    expect(data.recommended).toBeDefined();
+    expect(data.characteristics).toBeDefined();
+    expect(data.product_id).toEqual('1');
   });
 });
